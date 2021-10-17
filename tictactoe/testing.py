85cfe3df214c407277dcb15fbe9ae462fc7c424e
Michael Andrew Chan <67429929+michac789@users.noreply.github.com>
2021-10-17 10:10:07 +0800

    testing file added

---
+++ tictactoe/testing.py
@@ -0,0 +1,26 @@
+from tictactoe import player, actions, result, winner, terminal, utility
+
+def main():
+    board1 = [[None, None, None], [None, None, None], [None, None, None]]
+    board2 = [[None, None, None], [None, 'X', None], [None, None, None]]
+    board3 = [[None, 'X', None], ['O', None, None], ['O', 'X', None]]
+    board4 = [['X', None, None], ['X', 'O', None], [None, None, None]]
+    board5 = [['X', 'O', 'O'], ['X', 'O', None], [None, None, 'X']]
+    board6 = [['X', 'O', 'O'], ['X', 'X', None], ['O', None, 'X']]
+    board7 = [['X', None, 'O'], ['X', 'O', None], ['O', None, 'X']]
+    board8 = [['O', 'X', None], [None, 'X', None], ['O', 'X', None]]
+    board9 = [['X', None, 'X'], ['X', 'X', None], ['O', 'O', 'O']]
+    board10 = [['X', 'O', 'X'], ['X', 'O', 'X'], ['O', 'X', 'O']]
+    print(f"player 1: {player(board1)}, 2: {player(board2)}, 3: {player(board3)}, 4: {player(board4)}, 5: {player(board5)}")
+    print(f"action 1: {actions(board1)}, 2: {actions(board2)}, 3: {actions(board3)}, 4: {actions(board4)}, 5: {actions(board5)}, 10: {actions(board10)}")
+    print(f"result 1: {result(board1, (1, 2))}, 2: {result(board2, (0, 1))}, 3: {result(board3, (1,1))}, 4: {result(board4, (2,0))}")
+    #print(f"5: {result(board5, (0, 1))}") #raise index error here
+    print(f"winner 1: {winner(board1)}, 2: {winner(board2)}, 3: {winner(board3)}, 4: {winner(board4)}, 5: {winner(board5)}", end="")
+    print(f", 6: {winner(board6)}, 7: {winner(board7)}, 8: {winner(board8)}, 9: {winner(board9)}, 10: {winner(board10)}")
+    print(f"terminal 1: {terminal(board1)}, 2: {terminal(board2)}, 3: {terminal(board3)}, 4: {terminal(board4)}, 5: {terminal(board5)}", end="")
+    print(f", 6: {terminal(board6)}, 7: {terminal(board7)}, 8: {terminal(board8)}, 9: {terminal(board9)}, 10: {terminal(board10)}")
+    print(f"utility 1: {utility(board1)}, 2: {utility(board2)}, 3: {utility(board3)}, 4: {utility(board4)}, 5: {utility(board5)}", end="")
+    print(f", 6: {utility(board6)}, 7: {utility(board7)}, 8: {utility(board8)}, 9: {utility(board9)}, 10: {utility(board10)}")
+
+
+main()
--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -18,7 +18,7 @@ def initial_state():
 
 
 def player(board):
-    # Returns player who has the next turn on a board.
+    # Returns player who has the next turn on a board. (checked)
     x_pieces = 0
     o_pieces = 0
     for i in range(3):
@@ -29,14 +29,11 @@ def player(board):
                 o_pieces += 1
     if x_pieces == o_pieces:
         return X
-    elif x_pieces == o_pieces + 1:
-        return O
-    else:
-        return 'E'
+    return O
 
 
 def actions(board):
-    # Returns set of all possible actions (i, j) available on the board.
+    # Returns set of all possible actions (i, j) available on the board. (checked)
     valid_moves = set()
     for i in range(3):
         for j in range(3):
@@ -46,17 +43,17 @@ def actions(board):
 
 
 def result(board, action):
-    # Returns the board that results from making move (i, j) on the board.
+    # Returns the board that results from making move (i, j) on the board. (checked)
     board_temp = copy.deepcopy(board)
     if board_temp[action[0]][action[1]] == None:
         board_temp[action[0]][action[1]] = player(board)
     else:
-        raise 'Invalid Move'
+        raise IndexError()
     return board_temp
 
 
 def winner(board):
-    # Returns the winner of the game, if there is one.
+    # Returns the winner of the game, if there is one. (checked)
     for i in range(3):
         if board[i][0] == board[i][1] == board[i][2]:
             if board[i][2] == X:
@@ -72,20 +69,20 @@ def winner(board):
             if board[0][0 + i * 2] == board[1][1] == board[2][2 - i * 2]:
                 if board[1][1] == X:
                     return X
-                elif board[2][2] == O:
+                elif board[1][1] == O:
                     return O
     return None
 
 
 def terminal(board):
-    # Returns True if game is over, False otherwise.
+    # Returns True if game is over, False otherwise. (checked)
     if winner(board) != None or len(actions(board)) == 0:
         return True
     return False
 
 
 def utility(board):
-    # Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
+    # Returns 1 if X has won the game, -1 if O has won, 0 otherwise. (checked)
     if winner(board) == 'X':
         return 1
     elif winner(board) == 'O':
@@ -97,4 +94,23 @@ def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
-    raise NotImplementedError
+    if terminal(board) == True:
+        return utility(board)
+    possible_moves = actions(board)
+    if player(board) == X:
+        initial_score = -2
+        for moves in range(len(actions(board))):
+            board_simulation = result(board, list(possible_moves)[moves])
+            score = minimax(board_simulation)
+            if score > initial_score:
+                initial_score = score
+    elif player(board) == O:
+        initial_score = 2
+        for moves in range(len(actions(board))):
+            board_simulation = result(board, list(possible_moves)[moves])
+            score = minimax(board_simulation)
+            if score < initial_score:
+                initial_score = score
+    return utility(board)
+
+
