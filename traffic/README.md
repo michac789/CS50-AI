5decd9766cfb4419f5454d26175c15e9cede982b
Michael Andrew Chan <67429929+michac789@users.noreply.github.com>
2021-12-31 23:51:39 +0800

    make 3 layers of cnn

--- traffic/README.md
+++ traffic/README.md
@@ -2,4 +2,7 @@
 
 ## Experimentation Process
 
-5.35% accuracy
+In my first experiment, I tried only using one single layer for the convulational (32 filters, 3x3 kernel) and pooling layer (with 2,2 pool size), added with one single hidden layer and a dropout of 50% just by default (referring to the source code in handwriting.py). However, the result was bad as after 10 epoch, it was only 5.35% accurate which is extremely bad. As a result, I tried adding some more layers for both the convulational and pooling layer with the same default value as before. When there are two convulational layers and two pooling layer, the accuracy was preety good for about 
+
+
+90.57%
--- traffic/traffic.py
+++ traffic/traffic.py
@@ -79,6 +79,10 @@ def get_model():
     model = tf.keras.models.Sequential([
         tf.keras.layers.Conv2D(32, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)),
         tf.keras.layers.MaxPooling2D(pool_size = (2, 2)),
+        tf.keras.layers.Conv2D(32, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)),
+        tf.keras.layers.MaxPooling2D(pool_size = (2, 2)),
+        tf.keras.layers.Conv2D(32, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)),
+        tf.keras.layers.MaxPooling2D(pool_size = (2, 2)),
         tf.keras.layers.Flatten(),
         tf.keras.layers.Dense(128, activation="relu"),
         tf.keras.layers.Dropout(0.5),
