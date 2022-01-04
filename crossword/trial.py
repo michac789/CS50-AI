b70282e2a161be5f28c7b74477e0001268f4caa0
Michael Andrew Chan <67429929+michac789@users.noreply.github.com>
2022-01-04 18:26:55 +0800

    remove trial file, completed cs50 ai

--- crossword/trial.py
+++
@@ -1,5 +0,0 @@
-dict = {"a": 3, "b": 1, "c": 2, "d": 5, "e": 0}
-x = {k: v for k, v in sorted(dict.items(), key=lambda item: item[1])}
-print(x)
-y = [z[0] for z in sorted(dict.items(), key = lambda item: item[1])]
-print(y)
